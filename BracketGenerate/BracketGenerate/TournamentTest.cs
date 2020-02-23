c1a18a076d02ff7e26cf5dd9c0c67bc4f8b50284
Данил Посохов <lyudmila.gartman99@mail.ru>
2020-02-23 14:33:59 +0400

    TestGenerate

---
+++ BracketGenerate/BracketGenerate/TournamentTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentBracketGenerator.Helper;
+using TournamentBracketGenerator.Model;
+using BracketGenerate;
+
+namespace TournamentBracketGenerator
+{
+    //This class is too long actually. 
+    //It seems it breaks Open/Close and Single Responsobility Princibles
+    public class Tournament
+    {
+        public Tournament(List<Participant> totalParticipants)
+        {
+            TotalParticipants = totalParticipants;
+        }
+
+        //This is unnessecery if you return generated model. It will be easier to use, test and understand
+        public List<Participant> TotalParticipants;
+        //Use single seed of Random in entier class. Initializing new Random each time you need might create seeding issues
+        private Random rnd = new Random();
+
+
+        [ComVisible(true)]
+        //I dont like this personaly. It should return values.
+        //Return values are easier to test and alot easier to understand
+        //currently i am not quite sure what exatcly this crap does and how to use this dll
+        public void GenerateTournamemtBrackets()
+        {
+
+            RoundOperation FillRound = new RoundOperation();
+            FillRound.TournamentCalc(TotalParticipants);
+            FillRound.FillRounds(TotalParticipants);
+        }
+
+        
+    }
+}
---
+++ TestGenerate/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentBracketGenerator;
+using TournamentBracketGenerator.Model;
+using Tournaments.WPF.Model;
+
+namespace TestGenerate
+{
+    class Program
+    {
+        public List<Participant_Wrapper> CurrentParticipants { get; set; }
+        static void Main(string[] args)
+        {
+            var e = new TestGenerate.Program();
+            e.generate();
+        }
+        private void generate()
+        {
+            Console.WriteLine($"Турнирная таблица 1/32");
+            CurrentParticipants = new List<Participant_Wrapper>();
+            SeedCrap();
+            Tournament tour = new Tournament(CurrentParticipants.Select(x => x.Model).ToList());
+            tour.GenerateTournamemtBrackets();
+            for (int i = 0; i < tour.Rounds[0].Brackets.Count; i++)
+            {
+                Console.WriteLine(tour.Rounds[0].Brackets[i].BlueCorner.LastName + " - " + tour.Rounds[0].Brackets[i].RedCorner.LastName);
+            }
+            Console.WriteLine("1 / 16");
+            for (int i = 0; i < tour.Rounds[1].Brackets.Count; i++)
+            {
+                Console.WriteLine(tour.Rounds[1].Brackets[i].BlueCornerBracket.BlueCorner.LastName + "/" + tour.Rounds[1].Brackets[i].BlueCornerBracket.RedCorner.LastName + " - " + tour.Rounds[1].Brackets[i].RedCornerBracket.BlueCorner.LastName + "/" + tour.Rounds[1].Brackets[i].RedCornerBracket.RedCorner.LastName);
+            }
+            
+            Console.ReadKey();
+        }
+        private void SeedCrap()
+        {
+            for (int i = 0; i < 64; i++)
+            {
+                CurrentParticipants.Add
+                    (
+                        new Participant_Wrapper
+                        (
+                            _participant: new Participant()
+                            {
+                                Name = "SomeName",
+                                LastName = "SomeLastName",
+                                Patronymic = "SomePatronymic",
+                            },
+                            _id: +1
+                        )
+                    );
+            }
+        }
+    }
+}
