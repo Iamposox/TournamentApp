91e3a9ece798ccba4c449a145a3eda652ef50bc9
Данил Посохов <lyudmila.gartman99@mail.ru>
2020-02-28 00:21:45 +0400

    Add TeamClass

---
+++ BracketGenerate/BracketGenerate/NewModel/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketGenerate.NewModel
+{
+    public class Team
+    {
+        public string TeamName { get; set; }
+        public List<Participant> ParticipantName { get; set; }
+        private Guid _guid => Guid.NewGuid();
+        public Guid ID { get => _guid; }
+    }
+}
